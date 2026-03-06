c057fd65e59bac6169e04fad2bca8c29c9ce6982
Arnold Bernard <arnoldbernard1101@gmail.com>
2026-03-07 01:22:16 +0530

    Update backend.tf

--- backend/backend.tf
+++ backend/backend.tf
@@ -1,1 +1,7 @@
-
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "password123"
+}
